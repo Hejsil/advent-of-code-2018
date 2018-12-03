f9c39b0ef03b0b2224753703b55c9b6fc40bb27f
Jimmi Holst Christensen <jhc@liab.dk>
2018-12-03 11:06:58 +0100

    Simplified day1

--- src/day1_1.zig
+++ src/day1_1.zig
@@ -1,31 +1,29 @@
+const fun = @import("fun");
 const std = @import("std");
 
-const fmt = std.fmt;
-const heap = std.heap;
 const io = std.io;
-const mem = std.mem;
+const os = std.os;
+
+const scan = fun.scan.scan;
 
 pub fn main() !void {
     const stdin = &(try io.getStdIn()).inStream().stream;
     const stdout = &(try io.getStdOut()).outStream().stream;
-    var direct_allocator = heap.DirectAllocator.init();
-    const allocator = &direct_allocator.allocator;
-    defer direct_allocator.deinit();
+    var ps = io.PeekStream(1, os.File.InStream.Error).init(stdin);
 
-    var res: i64 = 0;
-    var running = true;
-    var line_buf = try std.Buffer.initSize(allocator, 0);
-    defer line_buf.deinit();
-    while (running) {
-        stdin.readUntilDelimiterBuffer(&line_buf, '\n', 10000) catch |err| switch (err) {
-            error.EndOfStream => running = false,
-            else => return err,
-        };
-        const line = mem.trimRight(u8, line_buf.toSlice(), "\r\n");
-        if (line.len == 0)
-            continue;
+    try stdout.print("{}\n", try sumLines(&ps));
+}
 
-        res += try fmt.parseInt(i64, line, 10);
-        try stdout.print("{}\n", res);
+fn sumLines(ps: var) !i64 {
+    var sum: i64 = 0;
+    while (scan(ps, "{}\n", struct {
+        i: i64,
+    })) |line| {
+        sum += line.i;
+    } else |err| switch (err) {
+        error.EndOfStream => {},
+        else => return err,
     }
+
+    return sum;
 }
--- src/day1_2.zig
+++ src/day1_2.zig
@@ -1,35 +1,38 @@
+const fun = @import("fun");
 const std = @import("std");
 
-const fmt = std.fmt;
 const heap = std.heap;
 const io = std.io;
 const mem = std.mem;
+const os = std.os;
+
+const scan = fun.scan.scan;
 
 pub fn main() !void {
     const stdin = &(try io.getStdIn()).inStream().stream;
     const stdout = &(try io.getStdOut()).outStream().stream;
+    var ps = io.PeekStream(1, os.File.InStream.Error).init(stdin);
+
     var direct_allocator = heap.DirectAllocator.init();
     const allocator = &direct_allocator.allocator;
     defer direct_allocator.deinit();
 
-    var res: i64 = 0;
+    try stdout.print("{}\n", try firstDupSum(allocator, &ps));
+}
+
+fn firstDupSum(allocator: *mem.Allocator, ps: var) !i64 {
     var map = std.AutoHashMap(i64, void).init(allocator);
-    var running = true;
-    var line_buf = try std.Buffer.initSize(allocator, 0);
-    defer line_buf.deinit();
-    while (running) {
-        stdin.readUntilDelimiterBuffer(&line_buf, '\n', 10000) catch |err| switch (err) {
-            error.EndOfStream => running = false,
-            else => return err,
-        };
-        const line = mem.trimRight(u8, line_buf.toSlice(), "\r\n");
-        if (line.len == 0)
-            continue;
+    defer map.deinit();
 
-        res += try fmt.parseInt(i64, line, 10);
+    var sum: i64 = 0;
+    while (true) {
+        const line = try scan(ps, "{}\n", struct {
+            i: i64,
+        });
+        sum += line.i;
 
-        const m_entry = try map.put(res, {});
+        const m_entry = try map.put(sum, {});
         if (m_entry) |entry|
-            try stdout.print("{}\n", entry.key);
+            return sum;
     }
 }
