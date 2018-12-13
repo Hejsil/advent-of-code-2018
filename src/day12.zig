7dea1d6dcf80564666c14342223c37d198f35e11
Jimmi Holst Christensen <jhc@liab.dk>
2018-12-13 13:04:01 +0100

    day12_2 with help from reddit

--- src/day12.zig
+++ src/day12.zig
@@ -25,43 +25,74 @@ pub fn main() !void {
     const notes = try readNotes(allocator, &ps);
     defer allocator.free(notes);
 
+    const note_mask = compileNotes(notes);
+
     var arena_state = heap.ArenaAllocator.init(allocator);
     const arena = &arena_state.allocator;
     defer arena_state.deinit();
 
-    //for (notes) |n| {
-    //    dumpMask(n.mask);
-    //}
-
+    // Sadly I had to look at reddit for this one. Did not think about the fact
+    // that the input would stabilize (not all inputs do though
+    // https://www.reddit.com/r/adventofcode/comments/a5eztl/comment/ebm4oec/)
+    // This solution is a little messy. Ooh well.
+    var sum_diff = std.ArrayList(isize).init(arena);
     var curr = init;
     var gen: usize = 0;
-    while (gen < 50000000000) : (gen += 1) {
-        //dumpState(curr);
+    var prev_sum: isize = sumState(init, 0);
+    var sum = prev_sum;
+    const repeating_diff = loop: while (gen < 50000000000) : (gen += 1) {
         if (gen % 1000 == 0) debug.warn("{}\n", gen);
-        curr = try simulate(arena, curr, notes);
-    }
-    //dumpState(curr);
+        curr = try simulate(arena, curr, note_mask);
 
-    var off = @divExact(@intCast(isize, curr.len - init.len), 2);
-    var sum: isize = 0;
-    for (curr) |c, i|
-        sum += if (c) @intCast(isize, i) - off else 0;
+        var off = @divExact(@intCast(isize, curr.len - init.len), 2) * 8;
+        const curr_sum = sumState(curr, off);
+        try sum_diff.append(curr_sum - prev_sum);
+        prev_sum = curr_sum;
+
+        // We just assume the sum has stabilize when the diff repeats 10 times
+        const diffs = sum_diff.toSlice();
+        const last = diffs[diffs.len - 1];
+        sum += last;
+
+        for (diffs) |_, i| {
+            const index = diffs.len - (i + 1);
+            debug.warn("{} ", diffs[index]);
+            if (diffs[index] != last)
+                break;
+            if (i == 10)
+                break :loop last;
+        }
+        debug.warn("\n");
+    } else blk: {
+        break :blk 0;
+    };
+    gen += 1;
+    sum += @intCast(isize, 50000000000 - gen) * repeating_diff;
 
     try stdout.print("{}\n", sum);
 }
 
-fn readInitialState(allocator: *mem.Allocator, ps: var) ![]bool {
-    var state = std.ArrayList(bool).init(allocator);
+fn readInitialState(allocator: *mem.Allocator, ps: var) ![]u8 {
+    var state = std.ArrayList(u8).init(allocator);
     defer state.deinit();
 
     _ = try scan(ps, "initial state: ", struct {});
 
-    while (true) switch (try ps.stream.readByte()) {
-        '#' => try state.append(true),
-        '.' => try state.append(false),
-        '\n' => return state.toOwnedSlice(),
-        else => return error.InvalidInput,
-    };
+    var i: usize = 0;
+    while (true) : (i += 1) {
+        if (state.len <= (i / 8))
+            try state.append(0);
+
+        const curr = &state.toSlice()[i / 8];
+
+        const c = try ps.stream.readByte();
+        switch (c) {
+            '#' => curr.* |= u8(1) << @intCast(u3, 8 - ((i % 8) + 1)),
+            '.' => {},
+            '\n' => return state.toOwnedSlice(),
+            else => return error.InvalidInput,
+        }
+    }
 }
 
 fn readNotes(allocator: *mem.Allocator, ps: var) ![]Note {
@@ -102,48 +133,68 @@ fn readNote(ps: var) !Note {
     };
 }
 
-fn simulate(allocator: *mem.Allocator, state: []const bool, notes: []const Note) ![]bool {
-    const size = blk: {
-        var size: usize = state.len;
-        for (state) |c, i| {
-            if (c or state[state.len - (i + 1)])
-                break;
-            size -= 1;
-        }
+fn simulate(allocator: *mem.Allocator, state: []const u8, note_mask: u32) ![]u8 {
+    const res = try allocator.alloc(u8, state.len + 2);
+    mem.set(u8, res, 0);
 
-        break :blk size;
-    };
-    const res = try allocator.alloc(bool, state.len + 4 * 2);
-    mem.set(bool, res, false);
+    for (res) |*b, i| {
+        const left = if (i < 2) u8(0) else state[i - 2];
+        const center = if (i < 1 or state.len <= i - 1) u8(0) else state[i - 1];
+        const right = if (state.len <= i) u8(0) else state[i];
+        const com: u16 = (u16(left) & 0b11) << 10 | u16(center) << 2 | ((u16(right) >> 6) & 0b11);
 
-    var mask: usize = 0;
-    for (res[2 .. res.len - 2]) |*c, i| {
-        mask <<= 1;
-        mask |= if (state.len <= i) u8(0) else @boolToInt(state[i]);
-        mask &= 0b11111;
-
-        for (notes) |n| {
-            if (mask == n.mask) {
-                c.* = n.result;
-                break;
-            }
+        comptime var j: usize = 0;
+        inline while (j < @sizeOf(u8) * 8) : (j += 1) {
+            const mask = (com >> @intCast(u5, j)) & u32(0b11111);
+            b.* |= u8(@boolToInt(note_mask & (u32(1) << @intCast(u5, mask)) != 0)) << @intCast(u3, j);
         }
     }
 
     return res;
 }
 
-fn dumpMask(mask: usize) void {
-    var i: usize = 0;
-    while (i < 5) : (i += 1) {
-        debug.warn("{c}", if (mask & (u8(1) << @intCast(u3, 5 - (i + 1))) != 0) u8('#') else u8('.'));
+fn sumState(state: []const u8, off: isize) isize {
+    var sum: isize = 0;
+    for (state) |c, i| {
+        var j: usize = 0;
+        const s = @sizeOf(u8) * 8;
+        while (j < s) : (j += 1) {
+            const is_set = (c & (u8(1) << @intCast(u3, s - (j + 1)))) != 0;
+            if (is_set) {
+                sum += @intCast(isize, i * s + j) - off;
+            }
+        }
     }
-    debug.warn("\n");
+
+    return sum;
 }
 
-fn dumpState(state: []const bool) void {
+fn compileNotes(notes: []const Note) u32 {
+    var res: u32 = 0;
+    for (notes) |n|
+        res |= u32(@boolToInt(n.result)) << @intCast(u5, n.mask);
+
+    return res;
+}
+
+fn sumF(comptime T: type, buf: []const T) isize {
+    var res: isize = 0;
+    for (buf) |item|
+        res += item;
+
+    return res;
+}
+
+fn dumpMask(mask: usize) void {
+    var i: usize = 0;
+    while (i < 8) : (i += 1) {
+        debug.warn("{c}", if (mask & (u8(1) << @intCast(u3, 8 - (i + 1))) != 0) u8('#') else u8('.'));
+    }
+}
+
+fn dumpState(state: []const u8) void {
     for (state) |s|
-        debug.warn("{c}", if (s) u8('#') else u8('.'));
+        dumpMask(s);
     debug.warn("\n");
 }
 
